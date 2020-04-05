2bd98bf731fe0c697e041d6f3764e5773d75856f
Jon Botros <joneb@Jons-MacBook-Pro-2.local>
2020-04-04 23:50:19 -0400

    not spagetti code

--- part1/graph.py
+++ part1/graph.py
@@ -2,37 +2,36 @@ import random
 from collections import deque
 
 class Node:
-    def __init__(self, data):
-        self.data = data
-        self.edges = set()
-        self.visited = False
-
+    def __init__(self, val):
+        self.val = val
+        self.edges = []
 
 class Graph:
     def __init__(self):
-        self.edges = {}
-        self.nodes = {}
+        self.nodes = set()
     
-    #todo
-
     def addNode(self, val):
         nodeToAdd = Node(val)
-        self.edges[nodeToAdd] = []
+        self.nodes.add(nodeToAdd)
 
     def addUndirectedEdge(self, nodeOne, nodeTwo):
         #probably buggy
-        self[nodeOne].edges.add(nodeTwo)
-        self[nodeTwo].edges.add(nodeOne)
+        if nodeTwo not in self.nodes[nodeOne]:
+            nodeOne.edges.append(nodeTwo)
+        if nodeOne not in self.nodes[nodeTwo]:
+            nodeTwo.edges.append(nodeOne)
 
     def removeUndirectedEdge(self, nodeOne, nodeTwo):
-        #probably buggy
-        self[nodeOne].edges.remove(nodeTwo)
-        self[nodeTwo].edges.remove(nodeOne)
+        if nodeTwo in self.nodes[nodeOne]:
+            self.nodes[nodeOne].remove(nodeTwo)
+        if nodeOne in self.nodes[nodeTwo]:
+            self.nodes[nodeTwo].remove(nodeOne)
 
     def getAllNodes(self):
         return self.nodes
 
-#todo main 
+
+#todo main
 
 def createRandomList(n):
     returnList = [random.randrange(0, n) for i in range(n)]
@@ -40,98 +39,93 @@ def createRandomList(n):
 
 def createRandomUnweightedGraph(n):
     randomGraph = Graph()
-
     #create random list
     randList = createRandomList(n)
 
     #not sure what i should do with this
-    """
-    for el in list:
+    
+    # for el in randList:
+    #     randomGraph.addNode(el)
+    #     if len(randomGraph.nodes) > 1:
+    #         randomIndex = random.randint(0, len(randomGraph.edges))
+    #         if randomIndex % 2 == 0:
+    #             randVert = randomGraph[randVert]
+    #             randomGraph.addUndirectedEdge(randVert, el)
 
-
-    """
     
 
 def createLinkedList(n):
     randomGraph = Graph()
     randList = createRandomList(n)
-    """
+    
     for i in range(n):
         curr = randList[i]
-        if graph is empty
+        el = Node(curr)
+        if len(randomGraph.nodes) == 0:
             randomGraph.addNode(el)
             prev = curr
         else:
-            prev = randList[i - 1]
-            randomeGraph.addNode(el)
+            randomGraph.addNode(el)
             randomGraph.addUndirectedEdge(el, prev)
-    """
+            prev = curr
+    
 
 def dfsIter(start, end):
-    """"
     s = []
     visitedArr = []
     for vertex in start.edges:
-        if vertex.visted == False:
-            vertex.visted = true
+        if vertex in visitedArr:
             visitedArr.append(vertex)
             s.append(vertex)
 
-            while S.isEmpty() != False:
-                cur = s.pop()
+            while s.isEmpty() != False:
+                curr = s.pop()
                 for vert in curr.edges:
-                    if vert.visted == False:
-                        vert.visted = true
+                    if vert in visitedArr:
+                        vert.visted = True
                         s.append(vert)
                         visitedArr.append(vertex)
 
-    """"
-
-def bftRecursive(g):
-    q = []
-    for v in g.edges:
-        if v.discovered == False:
-            q.append(v)
-            bfsHelperRec(g, q)
-    
 
 
-
-def bftHelperRec(g, q):
-    """
+def bftHelperRec(g, q, v):
     if q.isEmpty():
-        return null
+        return None
     
     curr = q.front()
 
     for vert in curr.edges:
-        if vert.visited == False:
-            vert.visted == True
+        if vert not in v:
             q.append(vert)
     
-    bfsHelperRec(g, q)
+    bftHelperRec(g, q)
 
-    """
+def bftRecursive(g):
+    q = []
+    visted = []
+    for v in g.edges:
+        if v not in visted:
+            q.append(v)
+            bftHelperRec(g, q, visted)
+    
+
 
 
 def bftIter(g):
-    """"
     q = []
     visitedArr = []
-    for vertex in start.edges:
-        if vertex.visted == False:
-            vertex.visted = true
+    for vertex in g.nodes:
+        if vertex not in visitedArr:
             visitedArr.append(vertex)
             q.append(vertex)
 
-            while S.isEmpty() != False:
-                cur = q.deque()
+            while len(q) != 0:
+                curr = q.deque()
                 for vert in curr.edges:
-                    if vert.visted == False:
-                        vert.visted = true
+                    if vert not in visitedArr:
                         q.append(vert)
                         visitedArr.append(vertex)
-    """"
+
 
 def bftRecLinkedList(g):
     bftRecursive(g)
