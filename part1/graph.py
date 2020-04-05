a65a058190ebca65e31c382a83a420012aac7203
Jon Botros <joneb@Jons-MacBook-Pro-2.local>
2020-04-05 14:35:04 -0400

    tested everthing besides 10000 elements

--- part1/graph.py
+++ part1/graph.py
@@ -9,23 +9,23 @@ class Node:
 class Graph:
     def __init__(self):
         self.nodes = set()
+        self.edges = {}
     
-    def addNode(self, val):
-        nodeToAdd = Node(val)
+    def addNode(self, nodeToAdd):
         self.nodes.add(nodeToAdd)
+        self.edges[nodeToAdd] = []
 
     def addUndirectedEdge(self, nodeOne, nodeTwo):
-        #probably buggy
-        if nodeTwo not in self.nodes[nodeOne]:
-            nodeOne.edges.append(nodeTwo)
-        if nodeOne not in self.nodes[nodeTwo]:
-            nodeTwo.edges.append(nodeOne)
+        if nodeTwo not in self.edges[nodeOne]:
+            self.edges[nodeOne].append(nodeTwo)
+        if nodeOne not in self.edges[nodeTwo]:
+            self.edges[nodeTwo].append(nodeOne)
 
     def removeUndirectedEdge(self, nodeOne, nodeTwo):
-        if nodeTwo in self.nodes[nodeOne]:
-            self.nodes[nodeOne].remove(nodeTwo)
-        if nodeOne in self.nodes[nodeTwo]:
-            self.nodes[nodeTwo].remove(nodeOne)
+        if nodeTwo in self.edges[nodeOne]:
+            self.edges[nodeOne].remove(nodeTwo)
+        if nodeOne in self.edges[nodeTwo]:
+            self.edges[nodeTwo].remove(nodeOne)
 
     def getAllNodes(self):
         return self.nodes
@@ -34,7 +34,7 @@ class Graph:
 #todo main
 
 def createRandomList(n):
-    returnList = [random.randrange(0, n) for i in range(n)]
+    returnList = [random.randrange(0, n * 100000) for i in range(n)]
     return returnList
 
 def createRandomUnweightedGraph(n):
@@ -42,15 +42,15 @@ def createRandomUnweightedGraph(n):
     #create random list
     randList = createRandomList(n)
 
-    #not sure what i should do with this
-    
-    # for el in randList:
-    #     randomGraph.addNode(el)
-    #     if len(randomGraph.nodes) > 1:
-    #         randomIndex = random.randint(0, len(randomGraph.edges))
-    #         if randomIndex % 2 == 0:
-    #             randVert = randomGraph[randVert]
-    #             randomGraph.addUndirectedEdge(randVert, el)
+    for i in range(n):
+        curr = randList[i]
+        if len(randomGraph.nodes) == 0:
+            randomGraph.addNode(curr)
+        else:
+            randomGraph.addNode(curr)
+            if i%2 == 0:
+                randomGraph.addUndirectedEdge(randList[random.randrange(0, i )], curr)
+    return randomGraph
 
     
 
@@ -60,71 +60,78 @@ def createLinkedList(n):
     
     for i in range(n):
         curr = randList[i]
-        el = Node(curr)
         if len(randomGraph.nodes) == 0:
-            randomGraph.addNode(el)
+            randomGraph.addNode(curr)
             prev = curr
         else:
-            randomGraph.addNode(el)
-            randomGraph.addUndirectedEdge(el, prev)
+            randomGraph.addNode(curr)
+            randomGraph.addUndirectedEdge(curr, prev)
             prev = curr
-    
 
-def dfsIter(start, end):
+    return randomGraph
+
+def dfsIter(start, end, graph):
     s = []
     visitedArr = []
-    for vertex in start.edges:
-        if vertex in visitedArr:
+    visitedArr.append(start)
+    for vertex in graph.edges[start]:
+        print(graph.edges[start])
+        if vertex not in visitedArr:
             visitedArr.append(vertex)
             s.append(vertex)
 
-            while s.isEmpty() != False:
+            while len(s) != 0:
                 curr = s.pop()
-                for vert in curr.edges:
-                    if vert in visitedArr:
-                        vert.visted = True
+                for vert in graph.edges[curr]:
+                    if vert not in visitedArr:
+                        visitedArr.append(vert)
                         s.append(vert)
-                        visitedArr.append(vertex)
+
+    return visitedArr
 
 
 
-def bftHelperRec(g, q, v):
-    if q.isEmpty():
-        return None
+def bftHelperRec(g, q, visted):
+    if len(q) == 0:
+        return visted
     
-    curr = q.front()
+    curr = q.pop(0)
 
-    for vert in curr.edges:
-        if vert not in v:
+    for vert in g.edges[curr]:
+        if vert not in visted:
+            visted.append(vert)
             q.append(vert)
+
     
-    bftHelperRec(g, q)
+    bftHelperRec(g, q, visted)
 
 def bftRecursive(g):
     q = []
     visted = []
     for v in g.edges:
         if v not in visted:
+            visted.append(v)
             q.append(v)
             bftHelperRec(g, q, visted)
-    
+    return visted 
 
 
 
 def bftIter(g):
     q = []
     visitedArr = []
-    for vertex in g.nodes:
+    for vertex in g.edges:
         if vertex not in visitedArr:
             visitedArr.append(vertex)
             q.append(vertex)
 
             while len(q) != 0:
-                curr = q.deque()
-                for vert in curr.edges:
+                curr = q.pop(0)
+                for vert in g.edges[curr]:
                     if vert not in visitedArr:
                         q.append(vert)
-                        visitedArr.append(vertex)
+                        visitedArr.append(vert)
+    return visitedArr
 
 
 def bftRecLinkedList(g):
@@ -132,3 +139,50 @@ def bftRecLinkedList(g):
 
 def bftIterLinkedList(g):
     bftIter(g)
+
+#testing
+
+print("adding nodes")
+mainGraph = Graph()
+mainGraph.addNode(1)
+mainGraph.addNode(2)
+mainGraph.addNode(4)
+mainGraph.addNode(5)
+mainGraph.addNode(7)
+mainGraph.addNode(8)
+mainGraph.addNode(12)
+mainGraph.addNode(44)
+mainGraph.addNode(21)
+mainGraph.addNode(49)
+mainGraph.addNode(401)
+mainGraph.addNode(67)
+mainGraph.addNode(81)
+mainGraph.addNode(99)
+
+print(mainGraph.getAllNodes())
+print('\n')
+
+print("adding and removing undirected edges")
+
+mainGraph.addUndirectedEdge(1,2)
+print(mainGraph.getAllNodes())
+print(mainGraph.edges)
+mainGraph.removeUndirectedEdge(1,2)
+print(mainGraph.edges)
+mainGraph.addUndirectedEdge(1,7)
+mainGraph.addUndirectedEdge(401,7)
+mainGraph.addUndirectedEdge(1,8)
+
+print("linked list and random graph")
+
+print(createRandomList(10))
+linkedListGraph = createLinkedList(7)
+print(linkedListGraph.edges)
+
+randomGraph = createRandomUnweightedGraph(7)
+print(randomGraph.edges)
+
+print(mainGraph.edges)
+print(dfsIter(1, 99, mainGraph))
+print(bftIter(mainGraph))
+print(bftRecursive(mainGraph))
